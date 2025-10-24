dcde6dfae721aed2e0842939bb0ef76c293ee35a
LaxmiPrasad Konduri <lucky0629@gmail.com>
2025-10-23 20:40:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Smooth scroll for navigation or section interactions (optional)
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
